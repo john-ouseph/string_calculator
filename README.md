b2cbb992da800ed8821c1a225d9c64fa16d50b77
john-ouseph <johnouseph152@gmail.com>
2024-07-08 09:07:39 +0530

    Initial failing test

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# string_calculator
+# string_calculator - A ruby project with rspec( TDD development)
+Ruby 3.0.0
 A calculator that calculates value from given string.
+Add fuctionality available, and it includes test cases
+ 1.returns 0 for an empty string
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,6 +3,10 @@ require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
   describe ".add" do
-    
+    context "given an empty string" do
+      it "returns 0" do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
   end
 end
